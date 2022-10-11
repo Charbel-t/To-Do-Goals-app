cdd411c92e866ef6a10aa1496f637aef68036dca
charbel-t <charbel2000.52@gmail.com>
2022-10-11 19:09:56 +0300

    added dynamic styles for error

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,22 +5,32 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
     const [enteredValue, setEnteredValue] = useState("");
+    const [isValid, setIsValid] = useState(true);
 
     const goalInputChangeHandler = (event) => {
+        if (event.target.value.trim().length > 0) setIsValid(true);
         setEnteredValue(event.target.value);
     };
 
     const formSubmitHandler = (event) => {
         event.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
         props.onAddGoal(enteredValue);
         setEnteredValue("");
     };
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
+            <div className={`form-control ${!isValid && "invalid"}`}>
                 <label>Course Goal</label>
-                <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
+                <input
+                    type="text"
+                    value={enteredValue}
+                    onChange={goalInputChangeHandler}
+                />
             </div>
             <Button type="submit">Add Goal</Button>
         </form>
